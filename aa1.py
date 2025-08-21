02316304e7770b438e6108ea63ddcab0fbd7fa00
Anjitha Methmadu <168812874+anjimeth2001@users.noreply.github.com>
2025-08-21 10:53:24 +0530

    Update aa1.py

--- aa1.py
+++ aa1.py
@@ -191,13 +191,13 @@ if post_file is not None:
             c = ws.cell(row=r, column=sum_idx)
             c.number_format = "0.00"
             c.font = Font(color="FFFFFF")
-            c.fill = PatternFill(start_color="000000", end_color="C6EFCE", fill_type="solid")  # Dark Blue
+            c.fill = PatternFill(start_color="C6EFCE", end_color="000000", fill_type="solid")  # Dark Blue
     if waste_gre_idx:
         for r in range(2, ws.max_row + 1):
             c = ws.cell(row=r, column=waste_gre_idx)
             c.number_format = "0.00"
             c.font = Font(color="FFFFFF")
-            c.fill = PatternFill(start_color="000000", end_color="C6EFCE", fill_type="solid")  # Dark Blue
+            c.fill = PatternFill(start_color="C6EFCE", end_color="000000", fill_type="solid")  # Dark Blue
     if action_idx:
         for r in range(2, ws.max_row + 1):
             c = ws.cell(row=r, column=action_idx)
@@ -215,6 +215,7 @@ if post_file is not None:
         file_name="modified_post.xlsx",
         mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     )
+
 
 
 
